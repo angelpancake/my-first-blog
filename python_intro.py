2e514257b7b60c5570c701ef27452b7625c326d6
angelpancake <maya@marello.net>
2019-05-04 18:59:10 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+##prendendo in input un numero di utenti, prendere il nome degli studenti tramite un ciclo e forma un elenco in ordine alfabetico
+
+#numeros = int(input("numero studenti "))
+#elenco = []
+#for i in range(numeros):
+#    names = input("nome studente ")
+#    elenco.append(names)
+#elenco.sort()
+#for nomen in elenco:
+#    print(nomen)
